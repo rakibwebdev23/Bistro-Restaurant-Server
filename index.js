fe9fa30bdd3d30cb0d64bcf9333c17d0051c6ac0
Rakib <rh.rakibhasan365@gmail.com>
2024-09-12 18:21:12 +0600

    create cart add and deleted

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const prot = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware
@@ -46,13 +46,22 @@ async function run() {
 
         // cart collection
         app.get("/carts", async (req, res) => {
-            const result = await cartCollection.find().toArray();
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray();
             res.send(result);
-        })
+        });
 
         app.post("/carts", async (req, res) => {
             const cartsItem = req.body;
             const result = await cartCollection.insertOne(cartsItem);
+            res.send(result);
+        });
+
+        app.delete("/carts/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
 
